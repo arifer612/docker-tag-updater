cf5c41f1a8a65fa59726af1866c926d7e8fe0e63
Arif Er <arifer612@proton.me>
2024-04-10 00:44:57 +0800

    feat!: rename ciu to image_version_checker
    This commit changes the name of the script from 'ciu' to 'image_version_checker'
    to properly reflect the purpose of the script. Indeed, there is no updating of a
    compose file in the script, nor are there plans for it. This project (and hence
    this script) is at the most only to check for and retrieve the most up-to-date
    semantic version tagged to a container image. The name of the parser is also
    updated to reflect this change.
    
    Besides these changes, documentation is added to the parser module to describe
    how it should be used.

--- docker_tag_updater/parser.py
+++ docker_tag_updater/parser.py
@@ -1,32 +1,55 @@
-"""The command line parser module."""
+"""The command line parser module.
+
+This module defines the parser that is used for the CLI tool (image-version-checker).
+
+"""
 
 import argparse
 
 
-ciu_parser = argparse.ArgumentParser(
-    prog="Compose Image Updater",
+ivc_parser = argparse.ArgumentParser(
     description="Check for updates to your Docker images' version tags.",
 )
+"""The argument parser for image-version-checker.
 
-ciu_parser.add_argument(
+Parameters
+----------
+image : str
+    The name of the container image, e.g., docker.io/alpine:3.19.1
+tag : str, default: latest
+    The base tag to compare the container image to, e.g., latest, edge, etc.
+rule : str, default: default
+    The rule used to parse the image tags/versions into a semantic version string for
+    comparison. The available options now are (default, docker.io, docker, lscr,
+    lscr.io, linuxserver)
+verbose : bool, default: False
+    Specify the verbosity of the inspector function.
+
+See also
+--------
+docker_tag_updater.helpers.regex_rules : For more information about rules.
+
+"""
+
+ivc_parser.add_argument(
     "image",
     help="""The name of the image that will be checked.
     Example: docker.io/alpine:3.19.1
     """)
-ciu_parser.add_argument(
+ivc_parser.add_argument(
     "-t",
     "--tag",
     help="The base tag to reference, e.g. latest, develop, alpine, etc.",
     default="latest",
 )
-ciu_parser.add_argument(
+ivc_parser.add_argument(
     "-r",
     "--rule",
     choices=["default", "lscr"],
     help="The semver regex rule.",
     default="default",
 )
-ciu_parser.add_argument(
+ivc_parser.add_argument(
     "-v",
     "--verbose",
     action="store_true",
