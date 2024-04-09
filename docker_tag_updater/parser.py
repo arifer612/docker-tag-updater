9bb845393648f5b8f160d0d14482b123db18fc5e
Arif Er <arifer612@proton.me>
2024-04-09 23:59:54 +0800

    feat!: remove subparsers, make parser basic
    The breaking change in this commit is that it removes both previously defined
    subparsers and replaces them with a single parser at the root level. The reason
    for the change is that for the purpose of this project, there is no need for the
    steps to be separated and to have different parsers to handle the different
    steps. A single parser to take in information of the container image and the
    base tag to compare it against is more than enough.

--- docker_tag_updater/parser.py
+++ docker_tag_updater/parser.py
@@ -8,26 +8,26 @@ ciu_parser = argparse.ArgumentParser(
     description="Check for updates to your Docker images' version tags.",
 )
 
-subparsers = ciu_parser.add_subparsers(required=True, dest='command')
-skopeo_parser = subparsers.add_parser(
-    "inspect", aliases=["i"], help="Inspect using Skopeo."
+ciu_parser.add_argument(
+    "image",
+    help="""The name of the image that will be checked.
+    Example: docker.io/alpine:3.19.1
+    """)
+ciu_parser.add_argument(
+    "-t",
+    "--tag",
+    help="The base tag to reference, e.g. latest, develop, alpine, etc.",
+    default="latest",
 )
-skopeo_parser.add_argument("image", help="The name of the image that will be checked.")
-skopeo_parser.add_argument(
-    "-t", "--tag", help="The base tag to reference, e.g. latest, develop, alpine, etc."
-)
-
-compare_parser = subparsers.add_parser(
-    "compare", aliases=["c"], help="Compare the semver."
-)
-compare_parser.add_argument(
-    "current", help="The current image string from the compose.yaml."
-)
-compare_parser.add_argument("newest", help="The newest image version (ciu skopeo).")
-compare_parser.add_argument(
+ciu_parser.add_argument(
     "-r",
     "--rule",
     choices=["default", "lscr"],
     help="The semver regex rule.",
     default="default",
+)
+ciu_parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
 )
